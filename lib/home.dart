b8f30d6a999081137d85864d6260e935c896cd20
DevanshArora312 <devansharora312@gmail.com>
2024-06-14 23:34:56 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[600],
+        title: const Text("Space App", style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+        ),),
+        centerTitle: true,
+      ),
+    );
+  }
+}
